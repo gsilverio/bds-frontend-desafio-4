651b2ad0cc865af7fd244312afe0fda6f2ace1ac
gsilverio <guilhermegsilverio@gmail.com>
2023-07-04 21:22:40 -0300

    Editing Style of React-Select

--- frontend/src/pages/Movies/index.tsx
+++ frontend/src/pages/Movies/index.tsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 import Select from "react-select";
 
 const Movies = () => {
+
+  //Custom Styles do Select para mudar diretamente o style do Style
+  const customStyles = {
+    singleValue:(provided:any)=>({
+      ...provided,
+      color:'#e1e1e1',
+      })
+  };
+
   const options = [{value:'chocolate', label:'Chocolate'},
   {value:'strawberry', label:'Strawberry'},
   {value:'vanilla', label:'Vanilla'}
@@ -13,6 +22,7 @@ const Movies = () => {
         <div className="movies-info-container">
         <div className="crud-select base-card">
         <Select
+        styles={customStyles}
           options={options}
           isClearable
           classNamePrefix="prefix-crud-select"
